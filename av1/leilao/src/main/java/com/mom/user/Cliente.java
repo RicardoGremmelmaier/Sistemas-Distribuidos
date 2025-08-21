24d99ccd95f5e294d5135e35c5d886ccec46eb3c
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2025-08-21 01:20:34 -0300

    feature: checkpoint implementation of client

--- av1/leilao/src/main/java/com/mom/user/Cliente.java
+++ av1/leilao/src/main/java/com/mom/user/Cliente.java
@@ -1,5 +1,29 @@
 package com.mom.user;
 
+import com.rabbitmq.client.*;
+
 public class Cliente {
+    private final String cliente_id;
+    private final String chave_publica;
+    private final String chave_privada;
+
+    public Cliente(String cliente_id, String chave_publica, String chave_privada) {
+        this.cliente_id = cliente_id;
+        this.chave_publica = chave_publica;
+        this.chave_privada = chave_privada;
+    }
+
+    public String getClienteId() {
+        return cliente_id;
+    }
+
+    public String getChavePublica() {
+        return chave_publica;
+    }
+
+    void publicarLance(){
+
+    }
+
 
 }
