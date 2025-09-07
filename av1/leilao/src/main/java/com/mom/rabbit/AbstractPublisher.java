80f3b2b4394f058b8de6ce371549b7ed342aaf41
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-09-07 16:02:18 -0300

    feature: created abstract pub and sub methods and abstract methods

--- av1/leilao/src/main/java/com/mom/rabbit/AbstractPublisher.java
+++ av1/leilao/src/main/java/com/mom/rabbit/AbstractPublisher.java
@@ -1,5 +1,27 @@
 package com.mom.rabbit;
 
-public abstract class AbstractPublisher {
+import java.io.IOException;
 
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public abstract class AbstractPublisher {
+    protected Channel channel;
+    protected String exchangeName = "leilao";
+
+    public AbstractPublisher() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(exchangeName, BuiltinExchangeType.DIRECT);
+        }
+    }
+
+    protected void publish(String routingKey, String message) throws IOException {
+        channel.basicPublish(exchangeName, routingKey, null, message.getBytes());
+        System.out.println("[PUB] Enviado '" + routingKey + "':'" + message + "'");
+    }
 }
--- av1/leilao/src/main/java/com/mom/rabbit/AbstractSubscriber.java
+++ av1/leilao/src/main/java/com/mom/rabbit/AbstractSubscriber.java
@@ -1,5 +1,34 @@
 package com.mom.rabbit;
 
-public abstract class AbstractSubscriber {
+import java.io.IOException;
+import java.util.List;
 
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public abstract class AbstractSubscriber {
+    protected Channel channel;
+    protected String exchangeName = "leilao";
+    public AbstractSubscriber(List<String> routingKeys) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        this.channel = connection.createChannel();
+        channel.exchangeDeclare(exchangeName, "direct", true);
+        String queueName = channel.queueDeclare().getQueue();
+
+        for (String key : routingKeys) {
+            channel.queueBind(queueName, exchangeName, key);
+        }
+
+        channel.basicConsume(queueName, true, (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println("[SUB] Recebido '" + delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+            handleMessage(message);
+        }, consumerTag -> {});
+    }
+
+    protected abstract void handleMessage(String message);
 }
