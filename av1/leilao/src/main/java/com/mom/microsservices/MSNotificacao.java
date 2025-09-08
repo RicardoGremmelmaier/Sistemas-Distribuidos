8e407c0fd35df5f019b9020afd7aad4962cdedaa
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2025-09-08 19:16:45 -0300

    refactor: change to JSON get method

--- av1/leilao/src/main/java/com/mom/microsservices/MSNotificacao.java
+++ av1/leilao/src/main/java/com/mom/microsservices/MSNotificacao.java
@@ -1,5 +1,7 @@
 package com.mom.microsservices;
 
+import org.json.JSONObject;
+
 import com.mom.rabbit.Publisher;
 import com.mom.rabbit.Subscriber;
 import com.mom.util.Lance;
@@ -28,7 +30,6 @@ public class MSNotificacao {
     }
 
     public void handleMensagem(String msg) {
-        System.out.println("[SUB] Recebido: '" + msg + "'");
 
         String routingKey = msg.split("]")[0].substring(1);
         String body = msg.split("]")[1].trim();
@@ -47,14 +48,13 @@ public class MSNotificacao {
     }
 
     public void handleLanceValidado(String msg) {
-        System.out.println("[SUB] Recebido lance validado: '" + msg + "'");
+        JSONObject json = new JSONObject(msg);
 
-        int leilaoId = Integer.parseInt(msg.split(",")[0].split(":")[1].trim());
-        int client_id = Integer.parseInt(msg.split(",")[1].split(":")[1].trim());
-        double valor = Double.parseDouble(msg.split(",")[2].split(":")[1].replace("}", "").trim());
+        int leilaoId = json.getInt("leilaoId");
+        int client_id = json.getInt("client_id");
+        double valor = json.getDouble("valor");
 
         Lance lance = new Lance(leilaoId, client_id, valor);
-
         try{
             publisher.publish("leilao." + leilaoId, lance.toString());
         }
@@ -64,12 +64,28 @@ public class MSNotificacao {
     }
 
     public void handleLeilaoVencedor(String msg) {
-        System.out.println("[SUB] Recebido leilão vencedor: '" + msg + "'");
+        JSONObject json = new JSONObject(msg);
+
+        int clienteId = json.getInt("clienteId");
+        int leilaoId = json.getInt("leilaoId");
+        double valor = json.getDouble("valor");
+
+        Lance lance = new Lance(leilaoId, clienteId, valor);
+        try{    
+            publisher.publish("leilao." + leilaoId, leilaoVencedorToString(lance));
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Erro ao publicar notificação de leilão vencedor", e);
+        } 
     }
 
     public void handleRoutingKeyInvalida(String msg) {
         System.out.println("[SUB] Roteamento inválido: '" + msg + "'");
         System.out.println("Ignorando mensagem...");
+    }
+
+    public String leilaoVencedorToString(Lance lance){
+        return "Leilão " + lance.getLeilaoId() + " vencido pelo cliente " + lance.getClienteId() + " com o valor de " + lance.getValor();
     }
 
     public static void main(String[] args) {
