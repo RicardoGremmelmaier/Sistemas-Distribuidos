32bb8fb1bd3845ee6d2175e363a2612108ad5fe8
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-10-28 13:00:05 -0300

    feat: added ms-lance initial files

---
+++ av3/leilao/backend/ms-lance/src/test/java/com/leilao/ms_lance/MsLanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leilao.ms_lance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
