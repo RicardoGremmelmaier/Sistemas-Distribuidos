355ee9b1c625acf6f80da9d2b13b3ff79ef81a5d
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-10-30 11:55:11 -0300

    feat: added lance service

---
+++ av3/leilao/backend/api-gateway/src/main/java/com/leilao/api_gateway/service/LanceService.java
@@ -0,0 +1,26 @@
+package com.leilao.api_gateway.service;
+
+import com.leilao.api_gateway.model.Lance;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class LanceService {
+
+    private final WebClient webClient;
+
+    public LanceService(@Value("${services.lance}") String baseUrl) {
+        this.webClient = WebClient.create(baseUrl);
+    }
+
+    public void enviarLance(Lance lance) {
+        webClient.post()
+                .uri("/lances")
+                .bodyValue(lance)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .subscribe();
+    }
+    
+}
