9c0886910a15204fbacf3070174a31f203b2d4a9
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-10-30 12:13:21 -0300

    feat: added cancel notifications in api gateway notification service

--- av3/leilao/backend/api-gateway/src/main/java/com/leilao/api_gateway/service/NotificationService.java
+++ av3/leilao/backend/api-gateway/src/main/java/com/leilao/api_gateway/service/NotificationService.java
@@ -1,5 +1,51 @@
 package com.leilao.api_gateway.service;
 
+import com.leilao.api_gateway.config.SseEmitterRegistry;
+import com.leilao.api_gateway.model.Evento;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Service
 public class NotificationService {
+
+    @Autowired
+    private SseEmitterRegistry registry;
+
+    public SseEmitter registrarCliente (Integer clienteID) {
+        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+        registry.addEmitter(clienteID, emitter);
+        emitter.onCompletion(() -> registry.removeEmitter(clienteID));
+        emitter.onTimeout(() -> registry.removeEmitter(clienteID));
+        emitter.onError(e -> registry.removeEmitter(clienteID));
+
+        return emitter;
+    }
+
+    public void notificarCliente (Integer clienteID, Evento evento) {
+        SseEmitter emitter = registry.getEmitter(clienteID);
+        if (emitter != null) {
+            try {
+                emitter.send(SseEmitter.event()
+                                       .name(evento.getTipo())
+                                       .data(evento));
+            } catch (Exception e) {
+                registry.removeEmitter(clienteID);
+            }
+        }
+    }
+
+    public void cancelarNotificacoes (Integer clienteID) {
+        SseEmitter emitter = registry.getEmitter(clienteID);
+        if (emitter != null) {
+            try {
+                emitter.complete();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                registry.removeEmitter(clienteID);
+            }
+        }
+    }
 
 }
