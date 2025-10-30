8d9ec934ebff38fe02be9d0e06612bd33c981c7b
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-10-30 12:47:51 -0300

    feat: added leilao controller

--- av3/leilao/backend/api-gateway/src/main/java/com/leilao/api_gateway/controller/LeilaoController.java
+++ av3/leilao/backend/api-gateway/src/main/java/com/leilao/api_gateway/controller/LeilaoController.java
@@ -1,5 +1,29 @@
 package com.leilao.api_gateway.controller;
 
+import com.leilao.api_gateway.model.Leilao;
+import com.leilao.api_gateway.service.LeilaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/leiloes")
 public class LeilaoController {
+
+    @Autowired
+    private LeilaoService leilaoService;
+
+    @PostMapping
+    public ResponseEntity<String> criarLeilao(@RequestBody Leilao leilao) {
+        leilaoService.criarLeilao(leilao);
+        return ResponseEntity.ok("Leilão criado com sucesso");
+    }
+
+    @GetMapping("/ativos")
+    public ResponseEntity<Object> listarLeiloesAtivos() {
+        return ResponseEntity.ok(leilaoService.listarLeiloes());
+    }
 
 }
