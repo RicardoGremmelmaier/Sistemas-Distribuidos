da3632e05a4218deed3fe59aa039f0625a2fbfbb
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-10-31 15:13:38 -0300

    feat: added lance result dto

--- av3/leilao/backend/ms-lance/src/main/java/com/leilao/ms_lance/model/ResultadoLance.java
+++ av3/leilao/backend/ms-lance/src/main/java/com/leilao/ms_lance/model/ResultadoLance.java
@@ -2,4 +2,19 @@ package com.leilao.ms_lance.model;
 
 public class ResultadoLance {
     
+    private boolean valido;
+    private String motivo;
+
+    public ResultadoLance() {}
+
+    public ResultadoLance(boolean valido, String motivo) {
+        this.valido = valido;
+        this.motivo = motivo;
+    }
+
+    public boolean isValido() {return valido; }
+    public void setValido(boolean valido) {this.valido = valido; }
+
+    public String getMotivo() {return motivo; }
+    public void setMotivo(String motivo) {this.motivo = motivo; }
 }
