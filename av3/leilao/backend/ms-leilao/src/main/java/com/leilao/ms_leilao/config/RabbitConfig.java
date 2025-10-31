22433f4ad5ea05e593b6702a40b84e064415a66b
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-10-30 22:16:38 -0300

    feat: added ms leilao rabbit config

--- av3/leilao/backend/ms-leilao/src/main/java/com/leilao/ms_leilao/config/RabbitConfig.java
+++ av3/leilao/backend/ms-leilao/src/main/java/com/leilao/ms_leilao/config/RabbitConfig.java
@@ -1,5 +1,15 @@
 package com.leilao.ms_leilao.config;
 
-public class RabbitConfig {
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class RabbitConfig {
+    public static final String EXCHANGE_LEILAO = "leilao.events";
+
+    @Bean
+    public TopicExchange leilaoExchange() {
+        return new TopicExchange(EXCHANGE_LEILAO);
+    }
 }
