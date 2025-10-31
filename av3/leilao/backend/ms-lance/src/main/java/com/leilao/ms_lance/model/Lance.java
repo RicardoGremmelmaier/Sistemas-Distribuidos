ae1ee703fe2b5cb6d13d5941822c36325c1e7503
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-10-31 15:05:03 -0300

    feat: added lance model dto

--- av3/leilao/backend/ms-lance/src/main/java/com/leilao/ms_lance/model/Lance.java
+++ av3/leilao/backend/ms-lance/src/main/java/com/leilao/ms_lance/model/Lance.java
@@ -1,5 +1,25 @@
 package com.leilao.ms_lance.model;
 
 public class Lance {
-    
+    private int clienteId;
+    private int leilaoId;
+    private double valor;
+
+    public Lance() {}
+
+    public Lance(int clienteId, int leilaoId, double valor) {
+        this.clienteId = clienteId;
+        this.leilaoId = leilaoId;
+        this.valor = valor;
+    }
+
+    public int getClienteId() {return clienteId; }
+    public void setClienteId(int clienteId) {this.clienteId = clienteId; }
+
+    public int getLeilaoId() {return leilaoId; }
+    public void setLeilaoId(int leilaoId) {this.leilaoId = leilaoId; }
+
+    public double getValor() {return valor; }
+    public void setValor(double valor) {this.valor = valor; }
+
 }
