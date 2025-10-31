d90e7b721df93e6a2731152f9d0da63ab9f957de
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-10-31 15:05:49 -0300

    feat: added lance event model dto

--- av3/leilao/backend/ms-lance/src/main/java/com/leilao/ms_lance/model/EventoLance.java
+++ av3/leilao/backend/ms-lance/src/main/java/com/leilao/ms_lance/model/EventoLance.java
@@ -1,5 +1,24 @@
 package com.leilao.ms_lance.model;
 
 public class EventoLance {
-    
+    private String tipo;
+    private String mensagem;
+    private Object dados;
+
+    public EventoLance() {}
+
+    public EventoLance(String tipo, String mensagem, Object dados) {
+        this.tipo = tipo;
+        this.mensagem = mensagem;
+        this.dados = dados;
+    }
+
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+
+    public String getMensagem() { return mensagem; }
+    public void setMensagem(String mensagem) { this.mensagem = mensagem; }
+
+    public Object getDados() { return dados; }
+    public void setDados(Object dados) { this.dados = dados; }
 }
