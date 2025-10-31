e9e4f9bbc1abb068591d46a28bf1f8c986ba7db4
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-10-31 16:10:30 -0300

    feat: added ms lance controller

--- av3/leilao/backend/ms-lance/src/main/java/com/leilao/ms_lance/controller/MsLanceController.java
+++ av3/leilao/backend/ms-lance/src/main/java/com/leilao/ms_lance/controller/MsLanceController.java
@@ -1,5 +1,27 @@
 package com.leilao.ms_lance.controller;
 
+import com.leilao.ms_lance.model.Lance;
+import com.leilao.ms_lance.model.ResultadoLance;
+import com.leilao.ms_lance.service.MsLanceService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/lances")
 public class MsLanceController {
-    
+
+    private final MsLanceService service;
+
+    public MsLanceController(MsLanceService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResultadoLance> enviarLance(@RequestBody Lance lance) {
+        ResultadoLance resultado = service.processarLance(lance);
+        if (resultado.isValido())
+            return ResponseEntity.ok(resultado);
+        else
+            return ResponseEntity.badRequest().body(resultado);
+    }
 }
