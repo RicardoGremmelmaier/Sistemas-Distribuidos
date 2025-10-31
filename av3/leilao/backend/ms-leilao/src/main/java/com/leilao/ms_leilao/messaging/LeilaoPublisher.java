2528860a3e4c2f23fd6d7efa3504a5858224964a
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-10-31 00:08:50 -0300

    feat: added leilao publisher

--- av3/leilao/backend/ms-leilao/src/main/java/com/leilao/ms_leilao/messaging/LeilaoPublisher.java
+++ av3/leilao/backend/ms-leilao/src/main/java/com/leilao/ms_leilao/messaging/LeilaoPublisher.java
@@ -1,5 +1,29 @@
 package com.leilao.ms_leilao.messaging;
 
+import com.leilao.ms_leilao.config.RabbitConfig;
+import com.leilao.ms_leilao.model.EventoLeilao;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class LeilaoPublisher {
 
+    
+    private final RabbitTemplate rabbitTemplate;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public LeilaoPublisher(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void publish(String routingKey, EventoLeilao evento) {
+        try {
+            String payload = mapper.writeValueAsString(evento);
+            rabbitTemplate.convertAndSend(RabbitConfig.EXCHANGE_LEILAO, routingKey, payload);
+            System.out.println("Publicado evento no RabbitMQ: " + routingKey + " -> " + payload);
+        } catch (Exception e) {
+            System.err.println("Erro ao publicar evento de leilao: " + e.getMessage());
+        }
+    }
 }
