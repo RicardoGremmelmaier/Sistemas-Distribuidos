f655bc040de5a51c25317a2247169e3a667fb228
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-10-30 23:47:32 -0300

    feat: added ms leilao auction event

--- av3/leilao/backend/ms-leilao/src/main/java/com/leilao/ms_leilao/model/EventoLeilao.java
+++ av3/leilao/backend/ms-leilao/src/main/java/com/leilao/ms_leilao/model/EventoLeilao.java
@@ -2,4 +2,25 @@ package com.leilao.ms_leilao.model;
 
 public class EventoLeilao {
 
+    private String tipo;
+    private String mensagem;
+    private Object dados;
+
+    public EventoLeilao() {
+    }
+
+    public EventoLeilao(String tipo, String mensagem, Object dados) {
+        this.tipo = tipo;
+        this.mensagem = mensagem;
+        this.dados = dados;
+    }
+
+    public String getTipo() {return tipo; }
+    public void setTipo(String tipo) {this.tipo = tipo; }
+
+    public String getMensagem() {return mensagem; }
+    public void setMensagem(String mensagem) {this.mensagem = mensagem; }
+
+    public Object getDados() {return dados; }
+    public void setDados(Object dados) {this.dados = dados; }
 }
