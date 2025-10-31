7bd74185adef2a92cce97fa3de96fe93feb6012b
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-10-31 15:46:45 -0300

    feat: added ms lance sub

--- av3/leilao/backend/ms-lance/src/main/java/com/leilao/ms_lance/messaging/LanceSubscriber.java
+++ av3/leilao/backend/ms-lance/src/main/java/com/leilao/ms_lance/messaging/LanceSubscriber.java
@@ -1,5 +1,23 @@
 package com.leilao.ms_lance.messaging;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leilao.ms_lance.model.EventoLance;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class LanceSubscriber {
-    
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @RabbitListener(queues = "mslance.queue")
+    public void receberMensagem(String mensagem) {
+        try {
+            EventoLance evento = mapper.readValue(mensagem, EventoLance.class);
+            System.out.println("[MSLance] recebeu evento: " + evento.getTipo());
+            // TODO: reagir a leilao_iniciado, leilao_finalizado, etc.
+        } catch (Exception e) {
+            System.err.println("[MSLance] Erro ao processar evento: " + e.getMessage());
+        }
+    }
 }
