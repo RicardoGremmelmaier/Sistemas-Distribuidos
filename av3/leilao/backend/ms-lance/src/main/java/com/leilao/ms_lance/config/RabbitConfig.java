f8f42fc047d7319e0e853ee4ffb42175d52c7a4d
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-10-31 15:03:32 -0300

    feat: added ms lance rabbit config

--- av3/leilao/backend/ms-lance/src/main/java/com/leilao/ms_lance/config/RabbitConfig.java
+++ av3/leilao/backend/ms-lance/src/main/java/com/leilao/ms_lance/config/RabbitConfig.java
@@ -1,5 +1,28 @@
 package com.leilao.ms_lance.config;
 
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitConfig {
-    
+
+    public static final String EXCHANGE_LEILAO = "leilao.events";
+    public static final String QUEUE_LANCES = "lance.queue";
+
+    @Bean
+    public TopicExchange leilaoExchange() {
+        return new TopicExchange(EXCHANGE_LEILAO);
+    }
+
+    @Bean
+    public Queue lanceQueue() {
+        return new Queue(QUEUE_LANCES);
+    }
+
+    @Bean
+    public Binding lanceBinding(TopicExchange leilaoExchange, Queue lanceQueue) {
+        return BindingBuilder.bind(lanceQueue).to(leilaoExchange).with("leilao.*");
+    }
+
 }
