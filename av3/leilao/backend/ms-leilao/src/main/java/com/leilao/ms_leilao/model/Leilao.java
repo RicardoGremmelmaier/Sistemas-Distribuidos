2d28824c4623f9c41f71d5a480f820e6f83f1749
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-10-30 22:23:17 -0300

    feat: added leilao model and enum

--- av3/leilao/backend/ms-leilao/src/main/java/com/leilao/ms_leilao/model/Leilao.java
+++ av3/leilao/backend/ms-leilao/src/main/java/com/leilao/ms_leilao/model/Leilao.java
@@ -1,5 +1,39 @@
 package com.leilao.ms_leilao.model;
 
+import java.time.OffsetDateTime;
+
 public class Leilao {
+    private int id;
+    private String nomeProduto;
+    private String descricao;
+    private double valorInicial;
+    private OffsetDateTime dataInicio;
+    private OffsetDateTime dataFim;
+    private LeilaoStatus status;
+    private Integer idVencedor;
+
+    public int getId() {return id; }
+    public void setId(int id) {this.id = id; }
+
+    public String getNomeProduto() {return nomeProduto; }
+    public void setNomeProduto(String nomeProduto) {this.nomeProduto = nomeProduto; }
+
+    public String getDescricao() {return descricao; }
+    public void setDescricao(String descricao) {this.descricao = descricao; }
+
+    public double getValorInicial() {return valorInicial; }
+    public void setValorInicial(double valorInicial) {this.valorInicial = valorInicial; }
+
+    public OffsetDateTime getDataInicio() {return dataInicio; }
+    public void setDataInicio(OffsetDateTime dataInicio) {this.dataInicio = dataInicio; }   
+
+    public OffsetDateTime getDataFim() {return dataFim; }
+    public void setDataFim(OffsetDateTime dataFim) {this.dataFim = dataFim; }
+
+    public LeilaoStatus getStatus() {return status; }
+    public void setStatus(LeilaoStatus status) {this.status = status; }
+
+    public Integer getIdVencedor() {return idVencedor; }
+    public void setIdVencedor(Integer idVencedor) {this.idVencedor = idVencedor; }
 
 }
---
+++ av3/leilao/backend/ms-leilao/src/main/java/com/leilao/ms_leilao/model/LeilaoStatus.java
@@ -0,0 +1,7 @@
+package com.leilao.ms_leilao.model;
+
+public enum LeilaoStatus {
+    PENDENTE,
+    ATIVO,
+    FINALIZADO
+}
