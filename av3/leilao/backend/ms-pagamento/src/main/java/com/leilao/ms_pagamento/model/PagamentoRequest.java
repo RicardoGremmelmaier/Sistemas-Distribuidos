0ac2136ca10895e6e4c0dd91e086e237aacd4a3e
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-11-07 13:24:11 -0300

    feat: added request for payment event dto

--- av3/leilao/backend/ms-pagamento/src/main/java/com/leilao/ms_pagamento/model/PagamentoRequest.java
+++ av3/leilao/backend/ms-pagamento/src/main/java/com/leilao/ms_pagamento/model/PagamentoRequest.java
@@ -1,5 +1,29 @@
 package com.leilao.ms_pagamento.model;
 
 public class PagamentoRequest {
-    
+    private int idLeilao;
+    private int idCliente;
+    private double valor;
+    private String moeda = "BRL";
+
+    public PagamentoRequest() {}
+
+    public PagamentoRequest(int idLeilao, int idCliente, double valor) {
+        this.idLeilao = idLeilao;
+        this.idCliente = idCliente;
+        this.valor = valor;
+    }
+
+    public int getIdLeilao() {return idLeilao; }
+    public void setIdLeilao(int idLeilao) {this.idLeilao = idLeilao; }
+
+    public int getIdCliente() {return idCliente; }
+    public void setIdCliente(int idCliente) {this.idCliente = idCliente; }
+
+    public double getValor() {return valor; }
+    public void setValor(double valor) {this.valor = valor; }
+
+    public String getMoeda() {return moeda; }
+    public void setMoeda(String moeda) {this.moeda = moeda; }
+
 }
