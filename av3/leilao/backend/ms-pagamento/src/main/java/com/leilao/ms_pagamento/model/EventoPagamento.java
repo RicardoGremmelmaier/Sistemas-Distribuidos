583515aa95b3f31821248a0c442f4cddd08085e1
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-11-07 13:14:00 -0300

    feat: added evento pagamento

--- av3/leilao/backend/ms-pagamento/src/main/java/com/leilao/ms_pagamento/model/EventoPagamento.java
+++ av3/leilao/backend/ms-pagamento/src/main/java/com/leilao/ms_pagamento/model/EventoPagamento.java
@@ -1,5 +1,25 @@
 package com.leilao.ms_pagamento.model;
 
 public class EventoPagamento {
+    private String tipo;
+    private String mensagem;
+    private Object dados;
+
+    public EventoPagamento() {}
+
+    public EventoPagamento(String tipo, String mensagem, Object dados) {
+        this.tipo = tipo;
+        this.mensagem = mensagem;
+        this.dados = dados;
+    }
+
+    public String getTipo() {return tipo; }
+    public void setTipo(String tipo) {this.tipo = tipo; }
+
+    public String getMensagem() {return mensagem; }
+    public void setMensagem(String mensagem) {this.mensagem = mensagem; }
+
+    public Object getDados() {return dados; }
+    public void setDados(Object dados) {this.dados = dados; }
     
 }
