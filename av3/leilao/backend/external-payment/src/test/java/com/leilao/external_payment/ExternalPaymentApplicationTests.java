28b77efa12afe818e59d10ee9375d3d16aa0cf8c
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-11-11 12:42:54 -0300

    feat: external payment init

---
+++ av3/leilao/backend/external-payment/src/test/java/com/leilao/external_payment/ExternalPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leilao.external_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
