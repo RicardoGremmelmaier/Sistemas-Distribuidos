1b9195a213de3699eba2ff3ef9606bb7112db3dc
RicardoGremmelmaier <ricardomaier10@gmail.com>
2025-11-12 16:11:56 -0300

    feat: added payment transaction dto

--- av3/leilao/backend/external-payment/src/main/java/com/leilao/external_payment/model/PaymentTransaction.java
+++ av3/leilao/backend/external-payment/src/main/java/com/leilao/external_payment/model/PaymentTransaction.java
@@ -1,5 +1,34 @@
 package com.leilao.external_payment.model;
 
+import java.util.Map;
+
 public class PaymentTransaction {
-    
+
+    private String idTransacao;
+    private int leilaoId;
+    private double valor;
+    private String status;
+    private Map<?, ?> dadosCliente;
+
+    public PaymentTransaction(String idTransacao, int leilaoId, double valor, String status, Map<?, ?> dadosCliente) {
+        this.idTransacao = idTransacao;
+        this.leilaoId = leilaoId;
+        this.valor = valor;
+        this.status = status;
+        this.dadosCliente = dadosCliente;
+    }
+
+    public String getIdTransacao() { return idTransacao; }
+    public int getLeilaoId() { return leilaoId; }
+    public double getValor() { return valor; }
+    public String getStatus() { return status; }
+    public Map<?, ?> getDadosCliente() { return dadosCliente; }
+
+    public void setStatus(String status) { this.status = status; }
+
+    @Override
+    public String toString() {
+        return String.format("Transação{id='%s', leilaoId=%d, valor=%.2f, status='%s'}",
+                idTransacao, leilaoId, valor, status);
+    }
 }
